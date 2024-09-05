7fc3ec29a73f9b9bbf9641a2a7adcf19bc6b744f
Tmkantichaaaa03 <momonut6627@gmail.com>
2024-09-05 15:28:22 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+console.log("hello javascript");
+console.log("hello Thursday");
+document.getElementById("text").innerHTML = "Kanticha Phuakkansee";
+document.write("Hello document write");
+
+window.alert("welcome to my page");
+
