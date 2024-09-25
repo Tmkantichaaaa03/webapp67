930af98c9442b827500ead94d71e875bdde0fb84
Tmkantichaaaa03 <momonut6627@gmail.com>
2024-09-25 19:38:04 +0700

    Add files via upload

---
+++ phpbasic/function26.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>writeMesses function</title>
+</head>
+<body>
+    <?php
+     function writeMesses() {
+        echo "this is first function for write Messes";
+        echo "Have a nice program";
+
+}//stop function
+
+//call  function
+writeMesses();
+    ?>
+</body>
+</html>
